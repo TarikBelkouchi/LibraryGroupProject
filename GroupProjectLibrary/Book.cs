a90ac8da57d876dc0cfdd855aa9fd2365534520b
Tarik <t.rock22@icloud.com>
2022-02-16 14:40:00 -0500

    Book class added

---
+++ GroupProjectLibrary/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace GroupProjectLibrary
+{
+	internal class Book
+	{
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public bool Status { get; set; }
+		public double DueDate { get; set; }
+
+		public Book(string _title, string _author, bool _status, double _duedate)
+		{
+			Title = _title;
+			Author = _author;
+			Status = _status;
+			DueDate = _duedate;
+
+		}
+	}
+}
+
